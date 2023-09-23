441097005955bcc5e67c9927186d5b0816ee982d
witherScript <chandlerscott123@gmail.com>
2023-09-22 23:16:26 -0500

    Initial implement, simple linked structure demo

---
+++ src/IntList.java
@@ -0,0 +1,10 @@
+public class IntList {
+    public int first;
+    public IntList rest;
+    public static void main(String[] args) {
+
+        IntList L = new IntList();
+        L.first = 5;
+        L.rest = null;
+    }
+}
